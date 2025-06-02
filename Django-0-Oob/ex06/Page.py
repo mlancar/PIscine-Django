40c172c04e0226c33484e42174613ec240636a60
Marine LANCAR <malancar@f4r4s4.clusters.42paris.fr>
2025-06-02 17:56:59 +0200

    c'est loooooong

--- Django-0-Oob/ex06/Page.py
+++ Django-0-Oob/ex06/Page.py
@@ -12,58 +12,76 @@ class Page():
     def __str__(self):
         return str(self.root)
 
-    def is_valid(self, element, last_elem, index):
+    def is_valid(self, element, current_path):
 
         list_elem = ["html", "head", "body", "title", "meta", "img", "table", "th", "tr", "td" , "ul", "ol", "li", "h1", "h2", "p", "div", "span", "hr", "br", "Text"]
 
 
         current_elem = element
-        print(element.tag)
-        print(f"index = {index}")
+        # print(element.tag)
+        # print(f"index = {index}")
 
-        print(f"current elem = {current_elem.tag}")
-        print(f"last elem = {last_elem.tag}")
+        # print(f"current elem = {current_elem.tag}")
+        # print(f"last elem = {last_elem.tag}")
         
+        # for elem in current_path:
+        #     print(f"current path = {elem}")
 
         if current_elem.tag not in list_elem:
             return False
-        if index == 0 and not isinstance(current_elem, elements.Html):
-            print("par la")
-            return False
-        elif index == 1 and (isinstance(last_elem, elements.Html)) and (not isinstance(current_elem, elements.Head)):
-            print("la")
-            return False
-        elif (isinstance(last_elem, elements.Head)) and (not isinstance(current_elem, elements.Title)):
-            print("ici")
-            return False
-        elif index == 2 and (isinstance(current_elem, elements.Body) and (not isinstance(last_elem, elements.Html))):
-            print("cc")
-            return False
-        
-        if current_elem.tag in list_elem:
-            if isinstance(current_elem, elements.Html):
-                list_elem.remove("html")
-            elif isinstance(current_elem, elements.Head):
-                list_elem.remove("head")
-            elif isinstance(current_elem, elements.Body):
-                list_elem.remove("body")
-            elif isinstance(current_elem, elements.Title):
-                list_elem.remove("title")
-
-        print("\n")
-
-        for i, elem in enumerate(current_elem.content):
-            if not self.is_valid(elem, current_elem, index + 1):
+        #HTML
+        if not current_path:
+            if not isinstance(current_elem, elements.Html):
+                return False    
+        # HEAD
+        elif isinstance(current_elem, elements.Head):
+            if "head" in current_path:
+                return False   
+        # TITLE
+        elif isinstance(current_elem, elements.Title):
+            if ((current_path[-1] == "head") and (elements.Title in current_path)):
                 return False
- 
+            elif "head" not in current_path or "title" in current_path:
+                return False
+        #BODY
+        elif isinstance(current_elem, elements.Body):
+            if ("head" not in current_path) or ("title" not in current_path):
+                return False
+            # elif (current_path[-1] != "")
+        #DIV
+        elif isinstance(current_elem, elements.Div):
+            if ("body" not in current_path):
+                return False
+        #TEXT
+        elif isinstance(current_elem, elements.Text)
+            if ((current_path[-1] == "title") or (current_path[-1] == "h1") or (current_path[-1] == "h2") or (current_path[-1] == "li") (current_path[-1] == "th") or (current_path[-1] == "td")):
+                if "text" in current_path:
+                    return False
+            # elif (current_path[-1] == "span")
+            #     return False
+        
+        #Ul/Ol/Li
+        elif (current_path[-1] == ("ul" or "ol")) and not isinstance(current_elem, elements.Li)
+            return False
+
+        elif 
+        #ADD ELEMENT
+        current_path.append(current_elem.tag)
+        # print("\n")
+        #RECURSSIOM
+        for i, elem in enumerate(current_elem.content):
+            if not self.is_valid(elem, current_path):
+                return False
         return True
 
+page = Page(elements.Html([elements.Head([elements.Title()]), elements.Body([elements.Div()])]))
 
-page = Page(elements.Html([elements.Head([elements.Title()]), elements.Body([elements.Div(), elements.Div()])]))
 print(page, "\n")
 try:
     index = 0
-    result = page.is_valid(page.root, page.root, index)
+    current_path = []
+    result = page.is_valid(page.root, current_path)
+    print(f"result: {result}")
 except Exception as e:
     print(e)
 
