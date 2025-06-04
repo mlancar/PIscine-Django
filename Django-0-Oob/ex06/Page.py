884e111593ab70fb9976cf840c733b13fb853209
Marine LANCAR <malancar@f4r7s13.clusters.42paris.fr>
2025-06-04 16:39:50 +0200

    phase de test

--- Django-0-Oob/ex06/Page.py
+++ Django-0-Oob/ex06/Page.py
@@ -12,7 +12,10 @@ class Page():
         self.root = root_elem
     
     def __str__(self):
-        return str(self.root)
+        if isinstance(self.root, Html):
+            return "<!DOCTYPE html>\n" + str(self.root)
+        else:
+            return str(self.root)
 
     def is_valid(self, element, current_path):
 
@@ -23,7 +26,6 @@ class Page():
         
         match current_elem:
             case Html():
-                
                 if len(current_elem.content) != 2:
                     return False
             
@@ -46,24 +48,53 @@ class Page():
             case Div():
                 if "body" not in current_path:
                     return False
-            
-            # case P():
-                # print("P")
-              
-            case Li():
-                if current_path[-1] != ("ul" or "ol"):
+                elif current_path[-1] not in ("body", "div"): #bizarre un peu
                     return False
+            
+            case Ul():
+                if current_path[-1] not in ("body", "div"):
+                    return False
+                elif not current_elem.content:
+                    return False
+            
+            case Ol():
+                if current_path[-1] not in ("body", "div"):
+                    return False
+                elif not current_elem.content:
+                    return False
+            case Li():
+                if current_path[-1] not in ("ul", "ol"):
+                    return False
+            
+            case Table():
+                if current_path[-1] not in ("body", "div"):
+                    return False
+            
+            case H1():
+                if current_path[-1] not in ("body", "div"):
+                    return False
+            
+            case H2():
+                if current_path[-1] not in ("body", "div"):
+                    return False
+
             case Tr():
                 if not current_elem.content:
                     return False
+            
             case Td():
                 if current_path[-1] != "tr":
                     return False
-            # case Table():
-            case Text():
-                if (current_path[-1] == "p") and (not isinstance(current_elem, Text)):
+
+            case Span():
+                if current_path[-1] not in ("body", "div"):
                     return False
-                if ((current_path[-1] == "title") or (current_path[-1] == "h1") or (current_path[-1] == "h2") or (current_path[-1] == "li") or (current_path[-1] == "th") or (current_path[-1] == "td")):
+            
+            case Text():
+                if (current_path[-1] != "p") and (current_path[-1] != "body") and (current_path[-1] != "title") and (current_path[-1] != "h1") and (current_path[-1] != "h2") and (current_path[-1] != "li") and (current_path[-1] != "th") and (current_path[-1] != "td") and (current_path[-1] != "span") and (current_path[-1] != "div"):
+                    print("ici")
+                    return False
+                elif ():
                     if "text" in current_path:
                         return False
                     else:
@@ -73,31 +104,31 @@ class Page():
                     current_path.append("text")
                     return True
 
-
         if current_path:
             match current_path[-1]:
                 case "span":
-                    if not isinstance(current_elem, str) and not isinstance(current_elem, P):
+                    if not isinstance(current_elem, Text) and not isinstance(current_elem, P):
                         return False    
-                case "ul" | "ol":
+                
+                case "ul":
                     if not isinstance(current_elem, Li):
                         return False
+                
                 case "table":
                     if not isinstance(current_elem, Tr):
                         return False
+                
                 case "tr":
                     if not isinstance(current_elem, Th) and not isinstance(current_elem, Td):
                         return False
+                
                 case "p":
-                    if not isinstance(current_elem, str):
+                    if not isinstance(current_elem, Text):
                         return False
-
-
         else:
             if not isinstance(current_elem, Html):
                 return False
 
-        # print(f"DEBUG: current_elem = {current_elem}, type = {type(current_elem)}")
         if isinstance(current_elem, Text):
             current_path.append("text")
             return True
@@ -109,17 +140,16 @@ class Page():
                 return False
         return True
 
+page = Page(Html([Head(Title()), Body([Ul(Li(Text("hello")))])]))
+
 # page = Page(Html([Head(Title()), Body(Text())]))
 
 # print(page, "\n")
-# try:
-#     index = 0
-#     current_path = []
-#     result = page.is_valid(page.root, current_path)
-#     print(f"result: {result}")
-# except Exception as e:
-#     traceback.print_exc()
-#     print(e)
-
-# print(result)
-# printf(Page(Html))
+try:
+    index = 0
+    current_path = []
+    result = page.is_valid(page.root, current_path)
+    print(f"result: {result}")
+except Exception as e:
+    traceback.print_exc()
+    print(e)
