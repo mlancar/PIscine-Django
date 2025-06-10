9a3d9a0763ea014c49fd903bd5025c3d28d12cdc
Marine LANCAR <malancar@f1br5s2.clusters.42paris.fr>
2025-06-10 15:16:26 +0200

    start

---
+++ Django-1-Lib/ex00/geohashing.py
@@ -0,0 +1,25 @@
+import sys
+# import antigravity
+
+def main(latitude, longitude):
+    int_latitude = int(latitude)
+    int_longitude = int(longitude)
+
+    
+        
+    
+if __name__=='__main__':
+    if len(sys.argv) == 3:
+        print("hello")
+        latitude = sys.argv[1]
+        # longitude = sys.argv[2]
+        latitude = 48.85341
+        longitude = 2.3488
+        if not isinstance(latitude, float) 
+            print('Expected Float number')
+        else if not isinstance(longitude, float) 
+            print('Expected Float number')
+        else:
+            main(latitude, longitude)
+    else:
+        print("Expected two arguments")
