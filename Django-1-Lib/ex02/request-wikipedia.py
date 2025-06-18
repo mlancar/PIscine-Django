91bb3047ab7b500d516c558f4ce90c3089db132e
malancar <malancar@malancar.myguest.virtualbox.org>
2025-06-18 17:57:47 +0200

    blablabla

--- Django-1-Lib/ex02/request-wikipedia.py
+++ Django-1-Lib/ex02/request-wikipedia.py
@@ -1,52 +1,68 @@
 import sys
 import requests
+import dewiki
 
-
-def get_title(request_name):
-    # print(f"request: {request_name}")
-    API_URL = "https://fr.wikipedia.org/w/api.php"
-    title = request_name
-
+def get_title(api_url, title_search):
 
     content_params = {
         "action": "query",
-        "list": "search",
+        "list": "search", #liste des resulats
         "format": "json",
-         "srsearch": title,
+        "srsearch": title_search, #terme cherche
     }
 
-    response = requests.get(API_URL, params=content_params)
+    response = requests.get(api_url, params=content_params)
     response.raise_for_status()
     data = response.json()
-
     results = data["query"]["search"]
+    
     if not results:
         print("Aucun résultat trouvé.")
         exit()
-    title = results[0]["title"]
+    title = results[0]["title"] #prend le titre du premier resultat
 
-def get_article(title):
+    return title
 
-    extract_params = {
-    "action": "query",
-    "prop": "extracts",
-    "titles": title,
-    "format": "json",
-    "explaintext": 1,
-    "redirects": 1
+def get_article(api_url, title):
+
+    params = {
+        "action": "query", #type de requete
+        "prop": "revisions", #dermiere version de l'article
+        "rvprop": "content", #donne juste le contenu, pas date ou auteur
+        "rvslots": "main", #recuperer le contenu principale de la page
+        "titles": title, #titre recherche
+        "format": "json", # retourne format json
+        "formatversion": 2 #version2 format json
     }
+   
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
+    data = response.json()
 
-    res2 = requests.get(API_URL, params=extract_params)
-    res2.raise_for_status()
-    data2 = res2.json()
+    if "query" not in data:
+        print("Aucune page trouvée ou erreur dans la requête.")
+        print("Réponse API :", data)
+        exit()
+    
+    page = data["query"]["pages"][0]
+    raw_wikitext = page["revisions"][0]["slots"]["main"]["content"]
+    clean_text = dewiki.from_string(raw_wikitext)
 
-    page = next(iter(data2["query"]["pages"].values()))
-    extract = page.get("extract", "[Aucun contenu trouvé]")
-
-    print(extract)
-
+    return clean_text
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Too many arguments")
-    title = get_title(sys.argv[1])
+        
+    api_url = "https://fr.wikipedia.org/w/api.php"
+    
+    title = get_title(api_url, sys.argv[1])
+
+    data = get_article(api_url, title)
+    formated_title = title.replace(" ", "_")
+    file_name = title + ".wiki"
+    try:
+        with open(file_name, "w", encoding="utf-8") as file:
+            file.write(data)
+    except Exception as e:
+        print(e)
---
+++ Django-1-Lib/ex03/roads_to_philosophy.py
@@ -0,0 +1,4 @@
+import sys
+import requests
+import dewiki
+import BeautifulSoup
