61121781b46d063d68b8f20465177e04eeb9348a
Marine LANCAR <malancar@f1r10s3.clusters.42paris.fr>
2025-06-23 20:41:36 +0200

    ca a l'air de fonctionne , faut encore tester

--- Django-1-Lib/ex03/roads_to_philosophy.py
+++ Django-1-Lib/ex03/roads_to_philosophy.py
@@ -2,34 +2,13 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-# def get_title(api_url, title_search):
-
-#     content_params = {
-#         "action": "query",
-#         "list": "search", #liste des resulats
-#         "format": "json",
-#         "srsearch": title_search, #terme cherche
-#     }
-
-#     response = requests.get(api_url, params=content_params)
-#     response.raise_for_status()
-#     data = response.json()
-#     results = data["query"]["search"]
-    
-#     if not results:
-#         print("Aucun résultat trouvé.")
-#         sys.exit(1)
-#     title = results[0]["title"] #prend le titre du premier resultat
-
-#     return title
-
-# def find_first_paragraphe(div):
-#     for element in div.children:
-#         element.find('p')
-#             print(f"element = {element}")
-#             return element
-#         # elif "homonymie" not in texte:
-#         #     return element
+def find_first_p(content_div):
+    #check pas help dans le lien/IPA
+    #test avec urine
+    for p in content_div.find_all('p'):
+        parent = p.parent
+        if parent.name == "div" and parent.get("class") == ['mw-content-ltr', 'mw-parser-output'] and p.get_text(strip=True):
+            return p
 
 def get_link(title):
     
@@ -37,45 +16,43 @@ def get_link(title):
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
 
-    content_div = soup.find('div', {'id': 'mw-content-text'}) #toutes les div de wiki contenant le premier paraggraphe s'appelle comme ca
+    content_div = soup.find('div', {'class': 'mw-content-ltr mw-parser-output'}) #toutes les div de wiki contenant le premier paraggraphe s'appelle comme ca
 
-    # first_paragraphe = find_first_paragraphe(content_div)
+
+    first_paragraphe = find_first_p(content_div)
     
-    # print("first_paragaphre: ", first_paragraphe)
+    # print(first_paragraphe)
+    #FAIRE UNE FONCTION PEUTETRE POUR TROUVER LE LIEN
+    link = first_paragraphe.find('a',  href=lambda href: href and '/wiki/' in href and '/wiki/Help' not in href and "/wiki/File:" not in href)
+    # print("link = ", link)
+    if link == None:
+        return None
+    new_title = link["title"].split(":")[-1]
 
-    link = content_div.find_all('a',  href=lambda href: href and href.startswith('https') and '/wiki/' in href)
-    
-    #check que le parent de <a> est l'id de la premiere div
-
-    print(link[0]["href"])
-    new_title = link[0]["title"].split(":")[-1]
-    if new_title == title:
-        print("test")
     return new_title
-
-def get_title(link):
-    print(link)
 
 def find_philosophy(title, road_to_philosophy):
 
-    
-    
+    # for title in road_to_philosophy:
+    #     print("title = ", title)
+
+    print(title)
+    road_to_philosophy.append(title)
+    title = get_link(title)
+    if title == None:
+        print("It leads to a dead end !", file=sys.stderr)
+        sys.exit(1)
+
     if title in road_to_philosophy:
         print("It leads to an infinite loop !", file=sys.stderr)
         sys.exit(1)
-    # elif:
-    #     print("It leads to a dead end !", file=sys.stderr)
-        #sys.exit(1)
 
-    if title != "https://en.wikipedia.org/wiki/Philosophy":
-        title = get_link(title)
-        print("new title = ", title)
-        #que faire si link existe pas
-        print(title)
-        road_to_philosophy.append(title)
+    if title != "Philosophy":
         find_philosophy(title, road_to_philosophy)
+    
 
     print(f"{len(road_to_philosophy)} roads from {road_to_philosophy[0]} to philosophy !")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
