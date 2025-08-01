d977c7952eb58414bcb375b455325de5deb6138e
Marine LANCAR <malancar@f1r11s17.clusters.42paris.fr>
2025-08-01 19:19:40 +0200

    je commence la tableau de couleurs

---
+++ Django-1-Base_Django/ex03/d04/ex01/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def django_page(request):
+    return render(request, 'ex01/django.html')
+
+def affichage_page(request):
+    return render(request, 'ex01/affichage.html')
+
+def templates_page(request):
+    return render(request, 'ex01/templates.html')
---
+++ Django-1-Base_Django/ex03/d04/ex03/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def init_component(request):
+
+    rows = 51
+    columns = 4
+    context = {
+        'rows': range(rows),
+        'columns': range(columns),
+    }
+    return render(request, 'ex03/index.html')
