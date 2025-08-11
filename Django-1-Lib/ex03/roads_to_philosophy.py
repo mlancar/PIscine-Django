43cecfe5746e50426babebeb43a973624f5f2a6a
Marine LANCAR <malancar@f2r3s7.clusters.42paris.fr>
2025-08-11 12:48:21 +0200

    je crois que c'est repare ??

--- Django-1-Lib/ex03/roads_to_philosophy.py
+++ Django-1-Lib/ex03/roads_to_philosophy.py
@@ -7,8 +7,22 @@ def find_first_p(content_div):
     #test avec urine
     for p in content_div.find_all('p'):
         parent = p.parent
-        if parent.name == "div" and parent.get("class") == ['mw-content-ltr', 'mw-parser-output'] and p.get_text(strip=True):
-            return p
+        # print(parent)
+        # print(p)
+        # print(parent.get("class"))
+        # print(p.find_parent(class_=["mw-content-ltr", "mw-parser-output"]))
+        # print(parent)
+        if p.find_parent(class_=["mw-content-ltr", "mw-parser-output"]):
+            if p.find('a', href=lambda href: href and '/wiki/' in href):
+                # print(p)
+                return p
+        # if parent.name == "div" and parent.get("class") == ['mw-content-ltr', 'mw-parser-output']:
+        #     # print(parent)
+        #     # print(p)
+        #     if p.find('a', href=True):
+        #         print("HELLO")
+        #         return p
+
 
 def get_link(title):
     
@@ -17,16 +31,16 @@ def get_link(title):
     soup = BeautifulSoup(res.text, 'html.parser')
 
     content_div = soup.find('div', {'class': 'mw-content-ltr mw-parser-output'}) #toutes les div de wiki contenant le premier paraggraphe s'appelle comme ca
-
-
+    # print(content_div)
     first_paragraphe = find_first_p(content_div)
-    
     # print(first_paragraphe)
-    #FAIRE UNE FONCTION PEUTETRE POUR TROUVER LE LIEN
-    print("link = ", link)
+    if first_paragraphe == None:
+        return None
     link = first_paragraphe.find('a',  href=lambda href: href and '/wiki/' in href and '/wiki/Help' not in href and "/wiki/File:" not in href)
+
     if link == None:
         return None
+
     new_title = link["title"].split(":")[-1]
 
     return new_title
@@ -39,6 +53,7 @@ def find_philosophy(title, road_to_philosophy):
     print(title)
     road_to_philosophy.append(title)
     title = get_link(title)
+
     if title == None:
         print("It leads to a dead end !", file=sys.stderr)
         sys.exit(1)
@@ -49,10 +64,6 @@ def find_philosophy(title, road_to_philosophy):
 
     if title != "Philosophy":
         find_philosophy(title, road_to_philosophy)
-    
-    #mettre ca ailleur
-    print(f"{len(road_to_philosophy)} roads from {road_to_philosophy[0]} to philosophy !")
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -60,3 +71,5 @@ if __name__ == "__main__":
         sys.exit(1)
     road_to_philosophy = []
     find_philosophy(sys.argv[1], road_to_philosophy)
+    print(f"{len(road_to_philosophy)} roads from {road_to_philosophy[0]} to philosophy !")
+
